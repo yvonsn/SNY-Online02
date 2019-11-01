4e7513b91781287f393619c218c42e19eac11506
Yvon <Yvon@LAPTOP-N8LMCARE>
2019-11-01 23:49:31 +0100

    Probando repositorio del ejercicio online02

---
+++ src/main/java/ysn/sny/online02/E01.java
@@ -0,0 +1,24 @@
+/*
+ * Permita pasar de pulgadas a centímetros. Para ello, el programa solicitará al usuario que introduzca el número de pulgadas 
+ * y a continuación mostrará cuantos centímetros son. (Nota: 1 pulgada son 2,54 cm).
+ */
+package ysn.sny.online02;
+
+/**
+ *
+ * @author Yvon
+ */
+public class E01 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        System.out.println("Ejercicio cambio de pulgadas a centímetros");
+        System.out.print("Ingrese cantidad en pulgadas: ");
+        
+    }
+    
+}
